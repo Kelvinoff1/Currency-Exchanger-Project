3616fcd3946f9dda9691e91e3607dd6e6eea4c2f
Artem Ponomarenko <kelvinoff40@gmail.com>
2025-03-23 13:08:27 +0200

    API connection

--- Currency-Exchanger-Project/Currency-Exchanger-Project/ContentView.swift
+++ Currency-Exchanger-Project/Currency-Exchanger-Project/ContentView.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+struct CurrencyCourse: Hashable, Codable {
+    let currencyCodeA: Int
+    let currencyCodeB: Int
+    let date: Int
+    let rateBuy: Double?
+    let rateSell: Double?
+    let rateCross: Double?
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
---
+++ Currency-Exchanger-Project/Currency-Exchanger-Project/ViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ViewModel.swift
+//  Currency-Exchanger-Project
+//
+//  Created by Artem Ponomarenko on 23.03.2025.
+//
+
+import Foundation
+import SwiftUI
+
+class ViewModel: ObservableObject {
+    @Published var courses: [CurrencyCourse] = []
+    
+    func fetch() {
+        
+        guard let url = URL(string: "https://api.monobank.ua/bank/currency") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else { return }
+            print(String(data: data, encoding: .utf8) ?? "Помилка отримання JSON")
+            // Convert to JSON
+            do {
+                let decodedResponse = try JSONDecoder().decode([CurrencyCourse].self, from: data)
+                DispatchQueue.main.async {
+                    self.courses = decodedResponse
+                }
+            } catch {
+                print("ПОМИЛКА: \(error)")
+            }
+        }
+       
+        task.resume()
+    }
+}
